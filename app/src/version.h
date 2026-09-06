82e557461efbc63d53a12316e9b4a1119800ee87
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-09-06 14:03:20 +0300

    1.3.1

--- app/src/version.h
+++ app/src/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
 // Keep the value a plain quoted literal, and the only digits on the line, otherwise it won't be parsed by app.pro correctly
-#define GG_VERSION "1.3.0"
+#define GG_VERSION "1.3.1"
